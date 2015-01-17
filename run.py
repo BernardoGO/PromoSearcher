01db8d767e5c3a68f25c9efc194fcb40289eba6b
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-01-17 04:19:25 -0200

    fix

--- run.py
+++ run.py
@@ -13,32 +13,6 @@ _FIND_ = ["smartphone"]
 
 
 
-def hardmob():
-    ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0 (compatible;)'
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)
-
-    browser.addheaders = [('User-agent', 'Firefox')]
-    start = 0
-    continuar = True
-    links = []
-
-    browser.open("http://www.hardmob.com.br/promocoes/")
-    html = browser.response().get_data()
-    browser.open("http://www.hardmob.com.br/promocoes/index2.html")
-    html += browser.response().get_data()
-    browser.open("http://www.hardmob.com.br/promocoes/index3.html")
-    html += browser.response().get_data()
-    browser.open("http://www.hardmob.com.br/promocoes/index4.html")
-    html += browser.response().get_data()
-
-
-    #print html
-    soup = BeautifulSoup.BeautifulSoup(html)
-    divList = soup(attrs={'class': 'title'})
-    return [divList, '']
-
-
 
 
 
--- sites.py
+++ sites.py
@@ -1,4 +1,10 @@
 __author__ = 'bernardo'
+import csv
+import mechanize
+import re
+import time
+import BeautifulSoup
+import smtplib
 
 def promoBit():
     ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0 (compatible;)'
@@ -25,3 +31,29 @@ def promoBit():
     prices = soup(attrs={'class': 'price'})
 
     return [divList, prices, "http://www.promobit.com.br/"]
+
+
+def hardmob():
+    ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0 (compatible;)'
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+
+    browser.addheaders = [('User-agent', 'Firefox')]
+    start = 0
+    continuar = True
+    links = []
+
+    browser.open("http://www.hardmob.com.br/promocoes/")
+    html = browser.response().get_data()
+    browser.open("http://www.hardmob.com.br/promocoes/index2.html")
+    html += browser.response().get_data()
+    browser.open("http://www.hardmob.com.br/promocoes/index3.html")
+    html += browser.response().get_data()
+    browser.open("http://www.hardmob.com.br/promocoes/index4.html")
+    html += browser.response().get_data()
+
+
+    #print html
+    soup = BeautifulSoup.BeautifulSoup(html)
+    divList = soup(attrs={'class': 'title'})
+    return [divList, '']
